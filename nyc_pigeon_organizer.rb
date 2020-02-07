ba9eb44a75ba302f8ab496353da78ee1a839a736
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-02-07 02:41:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ def nyc_pigeon_organizer(data)
   data.each { |outer_key, outer_value| 
     outer_value.each { |inner_key, inner_value|
       inner_value.each do |array_element|
-        new_pigeon_list[array_element] = {} unless  new_pigeon_list[array_element]
+        new_pigeon_list[array_element] = {} unless new_pigeon_list[array_element]
         new_pigeon_list[array_element][outer_key] = [] unless new_pigeon_list[array_element][outer_key]
         new_pigeon_list[array_element][outer_key] << inner_key.to_s
       end
